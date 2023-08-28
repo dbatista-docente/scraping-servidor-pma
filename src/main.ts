2955fe70be5a102d145040e6399813b066a72aa0
denerbatista <denerbatista@icloud.com>
2023-08-28 00:16:24 -0300

    feat: add partial config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { type Server } from 'http';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT ?? 3000;
+  const app = await NestFactory.create<INestApplication>(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', await app, document);
+  const server: Server = await app.listen(port, () => {
+    console.log(
+      `Application running on ${
+        !process.env.PORT ? `http://localhost:${port}` : `port ${port}`
+      }`,
+    );
+  });
+  process.on('SIGINT', () => {
+    server.close();
+    console.log('Finished Application');
+  });
 }
 bootstrap();
