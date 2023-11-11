177016898b4344bfe9d0ff141c1b4ef9b293cb5b
dbatista-docente <145720671+dbatista-docente@users.noreply.github.com>
2023-11-10 23:35:03 -0300

    Update contracheque.service.ts

--- src/contracheque/contracheque.service.ts
+++ src/contracheque/contracheque.service.ts
@@ -11,7 +11,7 @@ export class ContrachequeService {
       'janeiro',
       'fevereiro',
       'março',
-      'abril,',
+      'abril',
       'maio',
       'junho',
       'julho',
@@ -34,90 +34,93 @@ export class ContrachequeService {
     try {
       await page.goto('http://www.secom.aracruz.es.gov.br/servidor/login/');
       await page.waitForSelector('#matricula', { visible: true });
-    await page.type('#matricula', '32144');
-    await page.type('#senha', '32761562');
-    await page.click('#formLogin > div:nth-child(2) > input');
+      await page.type('#matricula', '32144');
+      await page.type('#senha', '32761562');
+      await page.click('#formLogin > div:nth-child(2) > input');
 
-    await page.waitForSelector(
-      '#servidor > div.internaTitulo > div > div.internaTitulo-titulo',
-      { visible: true },
-    );
+      await page.waitForSelector(
+        '#servidor > div.internaTitulo > div > div.internaTitulo-titulo',
+        { visible: true },
+      );
 
-    const datos = (
-      await page.$$eval('.pg-servidor-dados-resultado', (divs: any[]) =>
-        divs.map((div) => {
-          const monthCurrent = new Date().getMonth() + 1;
-          const stringMonths = [
-            'janeiro',
-            'fevereiro',
-            'março',
-            'abril,',
-            'maio',
-            'junho',
-            'julho',
-            'julho',
-            'agosto',
-            'setembro',
-            'outubro',
-            'setembro',
-            'novembro',
-            'dezembro',
-          ];
-          const text = div.innerText.toLowerCase().split('\n');
+      const datos = (
+        await page.$$eval('.pg-servidor-dados-resultado', (divs: any[]) =>
+          divs.map((div) => {
+            const monthCurrent = new Date().getMonth() + 1;
+            const stringMonths = [
+              'janeiro',
+              'fevereiro',
+              'março',
+              'abril',
+              'maio',
+              'junho',
+              'julho',
+              'julho',
+              'agosto',
+              'setembro',
+              'outubro',
+              'setembro',
+              'novembro',
+              'dezembro',
+            ];
+            const text = div.innerText.toLowerCase().split('\n');
 
-          const href = div.querySelector('a').href;
+            const href = div.querySelector('a').href;
 
-          return text[0] ===
-            `${stringMonths[monthCurrent]}/${String(new Date().getFullYear())}`
-            ? href
-            : null;
-        }),
-      )
-    ).filter((result) => result !== null);
+            return text[0] ===
+              `${stringMonths[monthCurrent]}/${String(
+                new Date().getFullYear(),
+              )}`
+              ? href
+              : null;
+          }),
+        )
+      ).filter((result) => result !== null);
 
-    for (let i = 0; i < datos.length; i++) {
-      try {
-        if (datos[i] != null) await page.goto(datos[i]);
-      } catch (error) {
-        console.error('Erro ao abrir o link do contracheque:', error);
+      for (let i = 0; i < datos.length; i++) {
+        try {
+          if (datos[i] != null) await page.goto(datos[i]);
+        } catch (error) {
+          console.error('Erro ao abrir o link do contracheque:', error);
+          continue;
+        }
+
+        let month: string | string[] = await page.$eval(
+          'body > div:nth-child(2) > table > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(2) > td',
+          (div) => div.textContent,
+        );
+
+        month = month.split(' ');
+
+        console.log('passei pelo screenshot');
+        await page.waitForSelector(
+          'body > div:nth-child(2) > table > tbody > tr > td:nth-child(1) > img',
+        );
+
+        const screenshotBuffer = await page.screenshot();
+
+        console.log(
+          `Contracheque ${month[1]}-${month[3]} gerado `,
+          Date.now(),
+        );
+
+        this._files = [
+          ...this._files,
+          [`${month[1]}-${month[3]}`, screenshotBuffer.toString('base64')],
+        ];
+
+        console.log(`${this._files.length} foram gerados.`);
+        console.log(this._files);
+
         continue;
       }
 
-      let month: string | string[] = await page.$eval(
-        'body > div:nth-child(2) > table > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(2) > td',
-        (div) => div.textContent,
-      );
+      await browser.close();
 
-      month = month.split(' ');
-
-      console.log('passei pelo screenshot');
-      await page.waitForSelector(
-        'body > div:nth-child(2) > table > tbody > tr > td:nth-child(1) > img',
-      );
-
-      const screenshotBuffer = await page.screenshot();
-
-      console.log(`Contracheque ${month[1]}-${month[3]} gerado `, Date.now());
-
-      this._files = [
-        ...this._files,
-        [`${month[1]}-${month[3]}`, screenshotBuffer.toString('base64')],
-      ];
-
-      console.log(`${this._files.length} foram gerados.`);
-      console.log(this._files);
-
-      continue;
-    }
-
-    await browser.close();
-
-    return this._files;
-  }
+      return this._files;
     } catch (error) {
       console.error('Erro ao abrir a página de login:', error);
       return [];
     }
-
-    
+  }
 }
